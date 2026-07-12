05dfb711d7bbaa38aa95db74d611f3e57708a0b2
loverHaoCheng <1501705072@qq.com>
2026-07-12 22:01:10 +0800

    修改：将备案号变为超链接

--- src/modules/legal/components/LegalFooter.tsx
+++ src/modules/legal/components/LegalFooter.tsx
@@ -1,9 +1,16 @@
 import styles from './LegalFooter.module.css';
 
-const recordLinks: { href: string; text: string }[] = [
+interface RecordLink {
+  href: string;
+  text: string;
+  ariaLabel: string;
+}
+
+const recordLinks: RecordLink[] = [
   {
     href: 'https://beian.miit.gov.cn/',
     text: '冀ICP备2026025428号-1',
+    ariaLabel: '冀ICP备2026025428号-1，点击跳转至工信部备案管理系统',
   },
 ];
 
@@ -12,11 +19,13 @@ export function LegalFooter() {
     <footer className={styles.footer}>
       {recordLinks.map((recordLink) => (
         <a
+          aria-label={recordLink.ariaLabel}
           className={styles.recordLink}
           href={recordLink.href}
           key={recordLink.text}
           rel="noopener noreferrer"
           target="_blank"
+          title={recordLink.ariaLabel}
         >
           {recordLink.text}
         </a>
